920ab05a3966dc72865a40e11a1d1a52a889eb41
cherishjainn <cherishjain56@gmail.com>
2026-06-02 21:27:39 +0530

    sol

---
+++ question5_reversestring.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseString(vector<char>& s) {
+      for(int i = 0  ; i<s.size()/2 ; i++){
+        swap(s[i] , s[s.size()-1-i]);
+      }  
+    }
