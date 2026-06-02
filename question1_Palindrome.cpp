e4689b4d556371f445a5ef3c8db76091401b2731
cherishjainn <cherishjain56@gmail.com>
2026-06-02 19:41:11 +0530

    sol

--- question1_Palindrome.cpp
+++ question1_Palindrome.cpp
@@ -1,3 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPalindrome(int x) {
+        if(x<0) return false ;
+        int target = x;
+        long long  reverse=0;
+        while(x>0){
+            reverse = reverse*10 + x%10;
+            x=x/10;
+        }
+        if(reverse != target) return false  ;
+        return true;
+    }
+
+int main(){
+    int x ;
+    cin>>x;
+   cout << isPalindrome(x);
+}    
