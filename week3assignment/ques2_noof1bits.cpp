c912b844cac4daeea763e0a23147fc356e116624
cherishjainn <cherishjain56@gmail.com>
2026-06-16 19:06:28 +0530

    sol2

---
+++ week3assignment/ques2_noof1bits.cpp
@@ -0,0 +1,12 @@
+ #include<bits/stdc++.h>
+using namespace std;
+
+ 
+ int hammingWeight(int n) {
+        int count = 0;
+       while(n){
+          n = n & (n-1);
+          count++;
+       } 
+       return count;
+    }
