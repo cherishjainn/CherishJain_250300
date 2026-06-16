c623b6271ecd2c4d7baca7da8b538e7c5f66f55d
cherishjainn <cherishjain56@gmail.com>
2026-06-16 19:09:18 +0530

    sol3

---
+++ week3assignment/ques3_singlenumber.cpp
@@ -0,0 +1,11 @@
+ #include<bits/stdc++.h>
+using namespace std;
+
+ 
+ int singleNumber(vector<int>& nums) {
+        int xorr = 0;
+        for(int i = 0; i<nums.size() ; i++){
+            xorr = xorr^nums[i];
+        }
+        return xorr;
+    }
