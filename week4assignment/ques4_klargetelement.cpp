1d7647b89010e7c681138be7f7deb21a96c7f62c
cherishjainn <cherishjain56@gmail.com>
2026-06-23 00:09:34 +0530

    sol4

---
+++ week4assignment/ques4_klargetelement.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> minh;
+        for(auto it:nums){
+            minh.push(it);
+            if(minh.size()>k){
+                minh.pop();
+            }
+        }
+        return minh.top();
+    }
