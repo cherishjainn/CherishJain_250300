c2b2d4dddc60cd1869a8b0798093dafb28c935cc
cherishjainn <cherishjain56@gmail.com>
2026-06-22 23:24:34 +0530

    sol3

---
+++ week3assignment/ques3_topKfrequentElements.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+  
+  vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+        for(auto it : nums){
+            mpp[it]++;
+        }
+ priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> minh;
+        for(auto it:mpp){
+           minh.push({it.second,it.first});
+           if(minh.size()>k){
+            minh.pop();
+           }
+        }
+        vector<int> ans;
+        while(minh.size()>0){
+            ans.push_back(minh.top().second);
+            minh.pop();
+        }
+        return ans ;
+    }
