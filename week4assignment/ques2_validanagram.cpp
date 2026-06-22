c18ccccfacf21d46f0466b4f011821228b24f897
cherishjainn <cherishjain56@gmail.com>
2026-06-22 23:16:17 +0530

    sol3

---
+++ week4assignment/ques2_validanagram.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+ bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        int   n = s.size();
+        int alphacount[27] ;
+        for(int i = 0 ; i<n ; i++){
+            alphacount[s[i]-'a']++;
+            alphacount[t[i]-'a']--;
+        }
+        for(auto i:alphacount){
+            if(i!=0) return false ;
+        }
+        return true;
+
+    }
