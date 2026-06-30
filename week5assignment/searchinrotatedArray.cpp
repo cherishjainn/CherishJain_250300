438e90cc0b7c2fd26796ea4e520be8e3e58b0226
cherishjainn <cherishjain56@gmail.com>
2026-07-01 00:12:06 +0530

    sol4

---
+++ week5assignment/searchinrotatedArray.cpp
@@ -0,0 +1,16 @@
+int search(vector<int>& nums, int target) {
+        int low = 0 , high = nums.size()-1;
+        while(low<=high){
+            int mid = (low + high)/2;
+             if(nums[mid]==target) return mid;
+             if(nums[mid]>=nums[low]){
+                if(target<nums[mid] && nums[low]<=target) high = mid-1;
+                else low = mid+1; 
+             }
+            else {
+                if(target>nums[mid] && nums[high]>=target) low = mid+1;
+                else high = mid-1; 
+            } 
+        }
+        return -1;
+    }
