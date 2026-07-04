49aace00f71f6b4ceac1dc11913ced0dd5dcf127
cherishjainn <cherishjain56@gmail.com>
2026-07-04 14:18:29 +0530

    sol9

---
+++ week6assignment/kthsymbol.cpp
@@ -0,0 +1,9 @@
+int kthGrammar(int n, int k) {
+        if(n==1) return 0;
+        if(k<=pow(2,n-2)){
+            return kthGrammar(n-1,k);
+        }
+    
+        return 1-kthGrammar(n-1,k-pow(2,n-2));
+        }
+    
