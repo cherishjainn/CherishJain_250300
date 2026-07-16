8456a33a4b2d7b96438e4308a2bfbb4faecd38c6
cherishjainn <cherishjain56@gmail.com>
2026-07-16 18:23:21 +0530

    soln

---
+++ week7assignment/searchinBST.cpp
@@ -0,0 +1,7 @@
+ TreeNode* searchBST(TreeNode* root, int val) {
+         if(root==NULL) return NULL;
+        if(root->val == val) return root;
+        TreeNode* l = searchBST(root->left,val);
+        TreeNode* r = searchBST(root->right,val);
+        return l?l:r;
+    }
